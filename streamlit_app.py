037365b40b20c16a804e830a25aadc2a5a285370
2025aa05836 <2025aa05836@wilp.bits-pilani.ac.in>
2026-02-15 14:05:32 +0530

    Upload functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.write('Hello World')
+# st.write('Hello World')
+
+st.set_page_config(page_title = 'File uploader')
+
+df = st.file_uploader(label = 'upload your dataset')
+
+if df:
+    df = pd.read_csv(df)
+    st.write(df.head())
